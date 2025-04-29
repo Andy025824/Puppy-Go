dd9fe7128f30333de0cf97d6a06c8885e5debd79
Andy025824 <65064485+Andy025824@users.noreply.github.com>
2025-04-29 11:54:33 +0530

    Latest Push

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Andy025824/Puppy-Go
 
 go 1.24.1
+
+require github.com/Andy025824/DogGo v0.0.0-20250429061245-c4c06a101a59
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppygo
 
+import (
+	"fmt"
+
+	doggo "github.com/Andy025824/DogGo"
+)
+
 func Bark() string {
-	return "Woof!"
+	return "Woof!====PuppyGo"
 }
 
 func Barks() string {
-	return "Woof! Woof! Woof!"
+	s3 := doggo.Bark1()
+	s4 := doggo.Bark2()
+	fmt.Println(s3)
+	fmt.Println(s4)
+	return "Woof! Woof! Woof!----PuppyGo"
 }
